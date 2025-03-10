4d4624b9c70543288888214da64a89c4507846a2
Dehit <82205289+DehitTrivedi@users.noreply.github.com>
2025-03-10 15:21:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("revealButton").addEventListener("click", function() {
+    let message = document.getElementById("hiddenMessage");
+    message.style.display = "block";
+});
